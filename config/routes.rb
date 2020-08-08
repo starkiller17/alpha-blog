d6784d1c7e0e6c9a55124a71df7498000c04ad4f
lalosaid <lalosaid@amazon.com>
2020-08-08 11:37:23 -0500

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # Gives all routes for the articles
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except:[:new]
 end
