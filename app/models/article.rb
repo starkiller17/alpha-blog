4a8671dccc02186a5385eeaba1adaf66f3d37b57
lalosaid <lalosaid@amazon.com>
2020-08-07 23:20:06 -0500

    Adding users dynamically displayed to views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 # class class_name inerhit class_inherited
 class Article < ApplicationRecord
+    belongs_to :user
     # Validates that title is part of the article
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
